ace0c5a0f7e78a98cc558c5f6767e1ad84f4ed2c
Kadir <kadirincedev@gmail.com>
2020-08-08 13:33:03 +0300

    Created New Button UI

--- BMI Calculator/ContentView.swift
+++ BMI Calculator/ContentView.swift
@@ -148,16 +148,28 @@ struct ContentView: View {
 
                 }) {
                     Text("Calculate")
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .font(.title)
-                        .padding(.horizontal, 60.0)
-                        .padding(.vertical, 20)
-                        .background(Color("button"))
-                        .cornerRadius(10)
-                        .foregroundColor(Color(red: 0.04, green: 0.05, blue: 0.13))
-                        .shadow(radius: 5)
-                        .padding(.vertical, 40)
-                        .padding(.horizontal, 20)
+                        .frame(width: screen.width * 0.50, height: screen.height * 0.07)
+                        .font(.system(size: 20, weight: .semibold))
+                        .background(
+                            ZStack {
+                                Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1))
+
+                                RoundedRectangle(cornerRadius: 16, style: .continuous)
+                                    .foregroundColor(.white)
+                                    .blur(radius: 4)
+                                    .offset(x: -8, y: -8)
+
+                                RoundedRectangle(cornerRadius: 16, style: .continuous)
+                                    .fill(
+                                        LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9019607843, green: 0.9294117647, blue: 0.9882352941, alpha: 1)), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                                    )
+                                    .padding(2)
+                                    .blur(radius: 2)
+                            }
+                        )
+                        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                        .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
+                        .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
                 }
             }
         }
@@ -167,6 +179,7 @@ struct ContentView: View {
         .onTapGesture {
             self.endTextEditing()
         }
+        .padding(.bottom, screen.height * 0.025)
     }
 }
 
@@ -180,7 +193,6 @@ extension View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewDevice("iPhone 11")
     }
 }
 
